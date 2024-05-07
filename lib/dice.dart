4feb0fb2207dca3960e818496d38b3ed840000f5
Hope Opus <hopeopus@gmail.com>
2024-05-07 23:57:31 +0100

    Dicee App

--- lib/dice.dart
+++ lib/dice.dart
@@ -3,16 +3,18 @@ import 'package:flutter/material.dart';
 void main() {
   return runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.red,
+        backgroundColor: const Color.fromARGB(255, 26, 26, 26),
         appBar: AppBar(
           title: const Center(
             child: Text(
-              'Dicee',
+              'Dicee App',
               style: TextStyle(
-                color: Colors.white,
-                fontSize: 35,
-              ),
+                  color: Colors.white,
+                  fontSize: 30,
+                  fontFamily: String.fromEnvironment('Montsterrat'),
+                  fontWeight: FontWeight.bold),
             ),
           ),
           backgroundColor: Color.fromARGB(255, 250, 8, 8),
@@ -33,16 +35,22 @@ class DicePage extends StatelessWidget {
           Expanded(
             child: Padding(
               padding: EdgeInsets.all(10.0),
-              child: Image(
-                image: AssetImage('images/dice1.png'),
+              child: TextButton(
+                onPressed: null,
+                child: Image(
+                  image: AssetImage('images/dice1.png'),
+                ),
               ),
             ),
           ),
           Expanded(
             child: Padding(
               padding: EdgeInsets.all(10.0),
-              child: Image(
-                image: AssetImage('images/dice2.png'),
+              child: TextButton(
+                onPressed: null,
+                child: Image(
+                  image: AssetImage('images/dice2.png'),
+                ),
               ),
             ),
           ),
